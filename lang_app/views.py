5796ffed5fd75ed9535fe1ec81ad2393cb90221a
stuart <stuart.miller60@gmail.com>
2018-10-27 13:53:09 +0100

    made changes for gathering data from volunteer

--- lang_app/views.py
+++ lang_app/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from lang_app.models import Card
 from random import randint
@@ -16,51 +16,51 @@ comp = TreeComparer()
 lem = Lemmatizer()
 
 # This is the normal index
-def index(request):
-
-    context = None
-    if request.method == 'GET':
-
-        question_number = request.GET.get('question_number')
-        if not question_number:
-
-            # don't use all cards. Only the cards with less than two uncommon words
-            all_cards = Card.objects.filter(sentence__uncommon_words_score__lt=2)
-
-            card = all_cards[randint(0, len(all_cards))]
-
-        else:
-            # use the question number to pick the next card
-            answered_correctly = request.GET.get('answered_correctly')
-            current_card = Card.objects.get(pk=question_number)
-            card = picker.pick(current_card, answered_correctly)
-
-        print(card.sentence)
-        data = card.ask_question()
-
-        context = {
-            'question_number': card.pk,
-            'question_data': data,
-            'required_words': lem.lemmatize(card)
-        }
-
-    if request.method == 'POST':
-        user_answer = request.POST.get('user_answer')
-
-        card = Card.objects.get(pk=request.POST.get('question_number'))
-        correct_bool = card.give_answer(user_answer)[0]
-
-        context = {
-            'show_answer': True,
-            'question_number': card.pk,
-            'question_data': card.ask_question(),
-            'correct_bool': correct_bool,
-            'user_answer': user_answer,
-            'chunk': card.chunk,
-            'chunk_translation': card.chunk_translation
-        }
-
-    return render(request, 'lang_app/template.html', context)
+# def index(request):
+#
+#     context = None
+#     if request.method == 'GET':
+#
+#         question_number = request.GET.get('question_number')
+#         if not question_number:
+#
+#             # don't use all cards. Only the cards with less than two uncommon words
+#             all_cards = Card.objects.filter(sentence__uncommon_words_score__lt=2)
+#
+#             card = all_cards[randint(0, len(all_cards))]
+#
+#         else:
+#             # use the question number to pick the next card
+#             answered_correctly = request.GET.get('answered_correctly')
+#             current_card = Card.objects.get(pk=question_number)
+#             card = picker.pick(current_card, answered_correctly)
+#
+#         print(card.sentence)
+#         data = card.ask_question()
+#
+#         context = {
+#             'question_number': card.pk,
+#             'question_data': data,
+#             'required_words': lem.lemmatize(card)
+#         }
+#
+#     if request.method == 'POST':
+#         user_answer = request.POST.get('user_answer')
+#
+#         card = Card.objects.get(pk=request.POST.get('question_number'))
+#         correct_bool = card.give_answer(user_answer)[0]
+#
+#         context = {
+#             'show_answer': True,
+#             'question_number': card.pk,
+#             'question_data': card.ask_question(),
+#             'correct_bool': correct_bool,
+#             'user_answer': user_answer,
+#             'chunk': card.chunk,
+#             'chunk_translation': card.chunk_translation
+#         }
+#
+#     return render(request, 'lang_app/template.html', context)
 
 
 # Ajax listener
@@ -83,78 +83,110 @@ def get_hint(request):
         return HttpResponse(word)
 
 # This is the test index that will record your answers
-# def index(request):
-#     '''
-#     The random numbers are read in from file each time. When you get a new question, you look for the last line
-#     in the user function file and then reference that line in the random numbers.
-#
-#     This is how you maintain the order.
-#     '''
-#
-#     rand_nums = [None,]   # dummy value to get counting from 1
-#     # Read in the data to a list
-#     with open("/home/stuart/PycharmProjects/workspaces/language_app_project/data/rand_nums.csv", 'r') as file:
-#         reader = csv.reader(file, delimiter='\n')
-#         for row in reader:
-#             rand_nums.append(row[0])
-#
-#     path = '/home/stuart/PycharmProjects/workspaces/language_app_project/data/user_function_c.csv'
-#     context = None
-#     if request.method == 'GET':
-#
-#         # get the number of the current question, so that we can stop the server here
-#         # if needed
-#         row_count = 1
-#         with open(path, 'r') as file:
-#             reader = csv.reader(file, delimiter=',')
-#             for row in reader:
-#                 row_count += 1
-#
-#         # Use the row number to get the pk from the list
-#         pk = rand_nums[row_count]
-#         card = Card.objects.get(pk=pk)
-#
-#         data = card.ask_question()
-#
-#         context = {
-#             'question_number': card.pk,
-#             'question_data': data,
-#         }
-#
-#         print("asking question: ", card.pk)
-#
-#     if request.method == 'POST':
-#
-#         user_answer = request.POST.get('user_answer')
-#
-#         card = Card.objects.get(pk=request.POST.get('question_number'))
-#         correct_bool = card.give_answer(user_answer)[0]
-#
-#         # use the parser to get the tree string for the user answer
-#         user_answer_tree_string = parser.parse(user_answer)[2]
-#
-#         # do the same to get the actual answer
-#         actual_answer_tree_string = parser.parse(card.chunk)[2]
-#
-#         score = comp.compare_tree_strings(user_answer_tree_string, actual_answer_tree_string)
-#         print("The score between these two answers was:", score)
-#
-#         # put the score and pk into the csv
-#         with open(path, 'a') as file:
-#             writer = csv.writer(file, delimiter=',')
-#             writer.writerow([card.pk, score])
-#
-#         context = {
-#             'show_answer': True,
-#             'question_number': card.pk,
-#             'question_data': card.ask_question(),
-#             'correct_bool': correct_bool,
-#             'user_answer': user_answer,
-#             'chunk': card.chunk,
-#             'chunk_translation': card.chunk_translation
-#         }
-#
-#     return render(request, 'lang_app/template.html', context)
+def index(request):
+    '''
+    The random numbers are read in from file each time. When you get a new question, you look for the last line
+    in the user function file and then reference that line in the random numbers.
+
+    This is how you maintain the order.
+    '''
+
+    rand_nums = [None,]   # dummy value to get counting from 1
+    # Read in the data to a list
+    with open("/home/stuart/PycharmProjects/workspaces/language_app_project/data/new_random_nums.csv", 'r') as file:
+        reader = csv.reader(file, delimiter='\n')
+        for row in reader:
+            rand_nums.append(row[0])
+
+    path = '/home/stuart/PycharmProjects/workspaces/language_app_project/data/new_user_function.csv'
+    context = None
+    if request.method == 'GET':
+
+        # get the number of the current question, so that we can stop the server here
+        # if needed
+        row_count = 1
+        with open(path, 'r') as file:
+            reader = csv.reader(file, delimiter=',')
+            for row in reader:
+                row_count += 1
+
+        # Use the row number to get the pk from the list
+        pk = rand_nums[row_count]
+        card = Card.objects.get(pk=pk)
+
+        data = card.ask_question()
+
+        context = {
+            'question_number': card.pk,
+            'question_data': data,
+        }
+
+        print("asking question: ", card.pk)
+
+    if request.method == 'POST':
+
+        if request.POST.get("second_post") == 'False':
+            # The first post takes the answer that they have entered and deals with it
+
+            user_answer = request.POST.get('user_answer')
+
+            card = Card.objects.get(pk=request.POST.get('question_number'))
+            correct_bool = card.give_answer(user_answer)[0]
+
+            # use the parser to get the tree string for the user answer
+            user_answer_tree_string = parser.parse(user_answer)[2]
+
+            # do the same to get the actual answer
+            actual_answer_tree_string = parser.parse(card.chunk)[2]
+
+            score = comp.compare_tree_strings(user_answer_tree_string, actual_answer_tree_string)
+            print("The score between these two answers was:", score)
+
+            # put the score and pk into the csv
+            with open(path, 'a') as file:
+                writer = csv.writer(file, delimiter=',')
+                writer.writerow([card.pk, score])
+
+            context = {
+                'show_answer': True,
+                'question_number': card.pk,
+                'question_data': card.ask_question(),
+                'correct_bool': correct_bool,
+                'user_answer': user_answer,
+                'chunk': card.chunk,
+                'chunk_translation': card.chunk_translation
+            }
+        else:
+            # the second post takes the difficulty rating and adds it to the other thing
+            rating = request.POST.get("rating")
+            question_no = request.POST.get("question_number")
+
+            # just read everything from the csv file into memory
+            data = []
+            with open(path, 'r') as file:
+                for row in csv.reader(file, delimiter=','):
+                    data.append(row)
+
+            # and then loop over add the stuff that you want to add
+            with open(path, 'w') as file:
+                writer = csv.writer(file, delimiter=',')
+                for d in data:
+                    if d[0] == question_no:
+                        writer.writerow([d[0], d[1], rating])
+                    else:
+                        writer.writerow(d)
+
+            # then I think you can just redirect to the get
+            redirect = "/?question_number={0}&answered_correctly={1}".format(question_no, request.POST.get("answered_correctly"))
+            return HttpResponseRedirect(redirect)
+
+
+
+
+
+
+
+    return render(request, 'lang_app/template.html', context)
 
 
 
--- scripts/generate_random_numbers.py
+++ scripts/generate_random_numbers.py
@@ -1,12 +1,30 @@
 # Generate some random numbers
 
 from random import shuffle
+# Django stuff
+import os
+import sys
+import django
+sys.path.append('/home/stuart/PycharmProjects/workspaces/language_app_project')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'language_app_project.settings'
+django.setup()
 
-path = "/home/stuart/PycharmProjects/workspaces/language_app_project/data/rand_nums.csv"
+from lang_app.models import Card, Sentence
+from utils.comparer.comparer import TreeComparer
+
+path = "/home/stuart/PycharmProjects/workspaces/language_app_project/data/new_random_nums.csv"
 with open(path, 'a') as file:
+	# get the first 200 numbers, shuffle then and write to file
 
-	numbers = [x for x in range(1, 201)]
-	shuffle(numbers)
-	for n in numbers:
-		file.write(str(n))
+	# numbers = [x for x in range(1, 201)]
+	# shuffle(numbers)
+	# for n in numbers:
+	# 	file.write(str(n))
+	# 	file.write('\n')
+
+	# Get the pks of the first 200 cards where the sentences have less than one uncommon word
+	all_cards = [c for c in Card.objects.filter(sentence__uncommon_words_score__lt=2)[:200]]
+	shuffle(all_cards)
+	for c in all_cards:
+		file.write(str(c.pk))
 		file.write('\n')
