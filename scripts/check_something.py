49dcc66a56233a487540c24adbcecdfeb8bb7b15
stuart <stuart.miller60@gmail.com>
2018-12-01 14:03:50 +0000

    bug fix

--- scripts/check_something.py
+++ scripts/check_something.py
@@ -2,11 +2,11 @@
 import os
 import sys
 import django
-sys.path.append('/home/stuart/PycharmProjects/workspaces/language_app_project')
+sys.path.append('/home/sdmiller/language_app_project')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'language_app_project.settings'
 django.setup()
 
-from lang_app.models import Question, Block, Session, UserState, Sentence
+from lang_app.models import Question, Block, Session, UserState, Sentence, QandA
 from utils.policies.policies import PolicyOne, PolicyTwo, PolicyThree
 import re
 from random import randint, shuffle
@@ -15,8 +15,20 @@ from tqdm import tqdm
 
 from django.contrib.auth.models import User
 
+# this_session = Session.objects.get(pk=17)
 
 
-import datetime
 
-print(datetime.datetime.now())
+
+
+user = User.objects.get(username='Susanna')
+
+# qanda = QandA.objects.filter(question__pk=312, block=this_block)
+
+
+# get the user state
+user_state = UserState.objects.get(user=user)
+block = user_state.current_session.current_block
+
+qanda = QandA.objects.get(question__pk=312, block=block)
+print(qanda)
